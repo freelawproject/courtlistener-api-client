dc3378b15fd7da8fc159b4d4bd6c9e88a57eab4d
Nathan Dahlberg <nadahlberg@gmail.com>
2026-02-16 09:35:01 -0500

    feat(api): add manually created options for search endpoints

--- scripts/generate_models.py
+++ scripts/generate_models.py
@@ -20,6 +20,456 @@ RELATED_ENDPOINT_MAP = {
 }
 
 
+COURT_CHOICES = json.loads(
+    Path(BASE_DIR / "scripts" / "court_choices.json").read_text()
+)
+
+STATE_CHOICES = [
+    {"value": "AL", "display_name": "Alabama"},
+    {"value": "AK", "display_name": "Alaska"},
+    {"value": "AS", "display_name": "American Samoa"},
+    {"value": "AZ", "display_name": "Arizona"},
+    {"value": "AR", "display_name": "Arkansas"},
+    {"value": "AA", "display_name": "Armed Forces Americas"},
+    {"value": "AE", "display_name": "Armed Forces Europe"},
+    {"value": "AP", "display_name": "Armed Forces Pacific"},
+    {"value": "CA", "display_name": "California"},
+    {"value": "CO", "display_name": "Colorado"},
+    {"value": "CT", "display_name": "Connecticut"},
+    {"value": "DE", "display_name": "Delaware"},
+    {"value": "DC", "display_name": "District of Columbia"},
+    {"value": "FL", "display_name": "Florida"},
+    {"value": "GA", "display_name": "Georgia"},
+    {"value": "GU", "display_name": "Guam"},
+    {"value": "HI", "display_name": "Hawaii"},
+    {"value": "ID", "display_name": "Idaho"},
+    {"value": "IL", "display_name": "Illinois"},
+    {"value": "IN", "display_name": "Indiana"},
+    {"value": "IA", "display_name": "Iowa"},
+    {"value": "KS", "display_name": "Kansas"},
+    {"value": "KY", "display_name": "Kentucky"},
+    {"value": "LA", "display_name": "Louisiana"},
+    {"value": "ME", "display_name": "Maine"},
+    {"value": "MD", "display_name": "Maryland"},
+    {"value": "MA", "display_name": "Massachusetts"},
+    {"value": "MI", "display_name": "Michigan"},
+    {"value": "MN", "display_name": "Minnesota"},
+    {"value": "MS", "display_name": "Mississippi"},
+    {"value": "MO", "display_name": "Missouri"},
+    {"value": "MT", "display_name": "Montana"},
+    {"value": "NE", "display_name": "Nebraska"},
+    {"value": "NV", "display_name": "Nevada"},
+    {"value": "NH", "display_name": "New Hampshire"},
+    {"value": "NJ", "display_name": "New Jersey"},
+    {"value": "NM", "display_name": "New Mexico"},
+    {"value": "NY", "display_name": "New York"},
+    {"value": "NC", "display_name": "North Carolina"},
+    {"value": "ND", "display_name": "North Dakota"},
+    {"value": "MP", "display_name": "Northern Mariana Islands"},
+    {"value": "OH", "display_name": "Ohio"},
+    {"value": "OK", "display_name": "Oklahoma"},
+    {"value": "OR", "display_name": "Oregon"},
+    {"value": "PA", "display_name": "Pennsylvania"},
+    {"value": "PR", "display_name": "Puerto Rico"},
+    {"value": "RI", "display_name": "Rhode Island"},
+    {"value": "SC", "display_name": "South Carolina"},
+    {"value": "SD", "display_name": "South Dakota"},
+    {"value": "TN", "display_name": "Tennessee"},
+    {"value": "WY", "display_name": "Wyoming"},
+    {"value": "TX", "display_name": "Texas"},
+    {"value": "UT", "display_name": "Utah"},
+    {"value": "VT", "display_name": "Vermont"},
+    {"value": "VI", "display_name": "Virgin Islands"},
+    {"value": "VA", "display_name": "Virginia"},
+    {"value": "WA", "display_name": "Washington"},
+    {"value": "WV", "display_name": "West Virginia"},
+    {"value": "WI", "display_name": "Wisconsin"},
+]
+
+
+SELECTION_METHOD_CHOICES = [
+    {
+        "value": "e_part",
+        "display_name": "Partisan Election",
+    },
+    {
+        "value": "appoine_non_parttment",
+        "display_name": "Non-Partisan Election",
+    },
+    {
+        "value": "a_pres",
+        "display_name": "Appointment (President)",
+    },
+    {
+        "value": "a_gov",
+        "display_name": "Appointment (Governor)",
+    },
+    {
+        "value": "a_legis",
+        "display_name": "Appointment (Legislature)",
+    },
+    {
+        "value": "a_judge",
+        "display_name": "Appointment (Judge)",
+    },
+    {
+        "value": "ct_trans",
+        "display_name": "Transferred (Court Restructuring)",
+    },
+]
+
+
+POLITICAL_AFFILIATION_CHOICES = [
+    {
+        "value": "d",
+        "display_name": "Democratic",
+    },
+    {
+        "value": "r",
+        "display_name": "Republican",
+    },
+    {
+        "value": "i",
+        "display_name": "Independent",
+    },
+    {
+        "value": "g",
+        "display_name": "Green",
+    },
+    {
+        "value": "l",
+        "display_name": "Libertarian",
+    },
+    {
+        "value": "f",
+        "display_name": "Federalist",
+    },
+    {
+        "value": "w",
+        "display_name": "Whig",
+    },
+    {
+        "value": "j",
+        "display_name": "Jeffersonian Republican",
+    },
+    {
+        "value": "u",
+        "display_name": "National Union",
+    },
+    {
+        "value": "z",
+        "display_name": "Reform Party",
+    },
+]
+
+
+OPINION_SEARCH_OPTIONS: dict[str, Any] = {
+    "endpoint": "/search/",
+    "actions": {
+        "POST": {
+            "cited_gt": {
+                "type": "integer",
+            },
+            "cited_lt": {
+                "type": "integer",
+            },
+        }
+    },
+    "filters": {
+        "type": {
+            "literal_value": "o",
+        },
+        "court": {
+            "type": "MultipleChoiceFilter",
+            "choices": COURT_CHOICES,
+        },
+        "q": {
+            "type": "CharFilter",
+        },
+        "semantic": {
+            "type": "BooleanFilter",
+        },
+        "case_name": {
+            "type": "CharFilter",
+        },
+        "judge": {
+            "type": "CharFilter",
+        },
+        "stat_Published": {
+            "type": "BooleanFilter",
+        },
+        "stat_Unpublished": {
+            "type": "BooleanFilter",
+        },
+        "stat_Errata": {
+            "type": "BooleanFilter",
+        },
+        "stat_Separate": {
+            "type": "BooleanFilter",
+        },
+        "stat_In_chambers": {
+            "type": "BooleanFilter",
+            "alias": "stat_In-chambers",
+        },
+        "stat_Relating_to": {
+            "type": "BooleanFilter",
+            "alias": "stat_Relating-to",
+        },
+        "stat_Unknown": {
+            "type": "BooleanFilter",
+        },
+        "filed_before": {
+            "type": "RelativeDateFilter",
+        },
+        "filed_after": {
+            "type": "RelativeDateFilter",
+        },
+        "citation": {
+            "type": "CharFilter",
+        },
+        "neutral_cite": {
+            "type": "CharFilter",
+        },
+        "docket_number": {
+            "type": "CharFilter",
+        },
+        "cited_gt": {
+            "type": "NumberFilter",
+        },
+        "cited_lt": {
+            "type": "NumberFilter",
+        },
+    },
+}
+
+
+RECAP_SEARCH_OPTIONS: dict[str, Any] = {
+    "endpoint": "/search/",
+    "actions": {
+        "POST": {
+            "document_number": {
+                "type": "integer",
+            },
+            "attachment_number": {
+                "type": "integer",
+            },
+        }
+    },
+    "filters": {
+        "type": {
+            "literal_value": "r",
+        },
+        "court": {
+            "type": "MultipleChoiceFilter",
+            "choices": COURT_CHOICES,
+        },
+        "q": {
+            "type": "CharFilter",
+        },
+        "semantic": {
+            "type": "BooleanFilter",
+        },
+        "available_only": {
+            "type": "BooleanFilter",
+        },
+        "case_name": {
+            "type": "CharFilter",
+        },
+        "description": {
+            "type": "CharFilter",
+        },
+        "docket_number": {
+            "type": "CharFilter",
+        },
+        "nature_of_suit": {
+            "type": "CharFilter",
+        },
+        "cause": {
+            "type": "CharFilter",
+        },
+        "filed_before": {
+            "type": "RelativeDateFilter",
+        },
+        "filed_after": {
+            "type": "RelativeDateFilter",
+        },
+        "entry_date_filed_before": {
+            "type": "RelativeDateFilter",
+        },
+        "entry_date_filed_after": {
+            "type": "RelativeDateFilter",
+        },
+        "document_number": {
+            "type": "NumberFilter",
+        },
+        "attachment_number": {
+            "type": "NumberFilter",
+        },
+        "assigned_to": {
+            "type": "CharFilter",
+        },
+        "referred_to": {
+            "type": "CharFilter",
+        },
+        "party_name": {
+            "type": "CharFilter",
+        },
+        "atty_name": {
+            "type": "CharFilter",
+        },
+    },
+}
+
+
+RECAP_DOCKET_SEARCH_OPTIONS: dict[str, Any] = RECAP_SEARCH_OPTIONS.copy()
+RECAP_DOCKET_SEARCH_OPTIONS["filters"]["type"] = {
+    "literal_value": "d",
+}
+
+
+RECAP_DOCUMENT_SEARCH_OPTIONS: dict[str, Any] = RECAP_SEARCH_OPTIONS.copy()
+RECAP_DOCUMENT_SEARCH_OPTIONS["filters"]["type"] = {
+    "literal_value": "rd",
+}
+
+
+JUDGE_SEARCH_OPTIONS: dict[str, Any] = {
+    "endpoint": "/search/",
+    "actions": {"POST": {}},
+    "filters": {
+        "type": {
+            "literal_value": "p",
+        },
+        "court": {
+            "type": "MultipleChoiceFilter",
+            "choices": COURT_CHOICES,
+        },
+        "q": {
+            "type": "CharFilter",
+        },
+        "semantic": {
+            "type": "BooleanFilter",
+        },
+        "name": {
+            "type": "CharFilter",
+        },
+        "born_before": {
+            "type": "RelativeDateFilter",
+        },
+        "born_after": {
+            "type": "RelativeDateFilter",
+        },
+        "dob_city": {
+            "type": "CharFilter",
+        },
+        "dob_state": {
+            "type": "ChoiceFilter",
+            "choices": STATE_CHOICES,
+        },
+        "school": {
+            "type": "CharFilter",
+        },
+        "appointer": {
+            "type": "CharFilter",
+        },
+        "selection_method": {
+            "type": "ChoiceFilter",
+            "choices": SELECTION_METHOD_CHOICES,
+        },
+        "political_affiliation": {
+            "type": "ChoiceFilter",
+            "choices": POLITICAL_AFFILIATION_CHOICES,
+        },
+    },
+}
+
+
+ORAL_ARGUMENT_SEARCH_OPTIONS: dict[str, Any] = {
+    "endpoint": "/search/",
+    "actions": {"POST": {}},
+    "filters": {
+        "type": {
+            "literal_value": "oa",
+        },
+        "court": {
+            "type": "MultipleChoiceFilter",
+            "choices": COURT_CHOICES,
+        },
+        "q": {
+            "type": "CharFilter",
+        },
+        "semantic": {
+            "type": "BooleanFilter",
+        },
+        "case_name": {
+            "type": "CharFilter",
+        },
+        "judge": {
+            "type": "CharFilter",
+        },
+        "argued_before": {
+            "type": "RelativeDateFilter",
+        },
+        "argued_after": {
+            "type": "RelativeDateFilter",
+        },
+        "docket_number": {
+            "type": "CharFilter",
+        },
+    },
+}
+
+
+SEARCH_OPTIONS: dict[str, Any] = {
+    "endpoint": "/search/",
+    "actions": {
+        "POST": {
+            **OPINION_SEARCH_OPTIONS["actions"]["POST"],
+            **RECAP_SEARCH_OPTIONS["actions"]["POST"],
+            **JUDGE_SEARCH_OPTIONS["actions"]["POST"],
+            **ORAL_ARGUMENT_SEARCH_OPTIONS["actions"]["POST"],
+        }
+    },
+    "filters": {
+        **OPINION_SEARCH_OPTIONS["filters"],
+        **RECAP_SEARCH_OPTIONS["filters"],
+        **JUDGE_SEARCH_OPTIONS["filters"],
+        **ORAL_ARGUMENT_SEARCH_OPTIONS["filters"],
+    },
+}
+
+
+SEARCH_OPTIONS["filters"]["type"] = {
+    "type": "ChoiceFilter",
+    "choices": [
+        {
+            "value": "o",
+            "display_name": "Opinion",
+        },
+        {
+            "value": "r",
+            "display_name": "Recap",
+        },
+        {
+            "value": "rd",
+            "display_name": "Document",
+        },
+        {
+            "value": "d",
+            "display_name": "Docket",
+        },
+        {
+            "value": "p",
+            "display_name": "Judge",
+        },
+        {
+            "value": "oa",
+            "display_name": "Oral Argument",
+        },
+    ],
+}
+
+
 def to_title_case(s: str) -> str:
     return (
         s.replace("_", " ")
@@ -161,6 +611,15 @@ def get_endpoint_data(cache_path: str | Path | None = None) -> dict[str, Any]:
         options = get_options()
         if cache_path is not None:
             cache_path.write_text(json.dumps(options, indent=2))
+
+    # Add custom endpoints
+    options["opinion-search"] = OPINION_SEARCH_OPTIONS
+    options["recap-search"] = RECAP_SEARCH_OPTIONS
+    options["recap-docket-search"] = RECAP_DOCKET_SEARCH_OPTIONS
+    options["recap-document-search"] = RECAP_DOCUMENT_SEARCH_OPTIONS
+    options["judge-search"] = JUDGE_SEARCH_OPTIONS
+    options["oral-argument-search"] = ORAL_ARGUMENT_SEARCH_OPTIONS
+    options["search"] = SEARCH_OPTIONS
 
     # Assemble endpoints data
     endpoints: dict[str, Any] = {}
