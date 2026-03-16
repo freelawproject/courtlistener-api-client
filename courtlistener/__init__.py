37315527a0f0320483cfe49406fb185788818fa7
Nathan Dahlberg <nadahlberg@gmail.com>
2026-03-16 14:13:18 -0400

    feat(init): export SearchAlerts and DocketAlerts

--- courtlistener/__init__.py
+++ courtlistener/__init__.py
@@ -1,4 +1,10 @@
+from courtlistener.alerts import DocketAlerts, SearchAlerts
 from courtlistener.citation_lookup import CitationLookup
 from courtlistener.client import CourtListener
 
-__all__ = ["CitationLookup", "CourtListener"]
+__all__ = [
+    "CitationLookup",
+    "CourtListener",
+    "DocketAlerts",
+    "SearchAlerts",
+]
