22aaa468417686d7b3b4f8a88a19f27ede2df0e3
Nathan Dahlberg <nadahlberg@gmail.com>
2026-08-01 16:04:27 -0400

    feat(package): only export CL client to package root

--- courtlistener/__init__.py
+++ courtlistener/__init__.py
@@ -1,12 +1,5 @@
-from courtlistener.alerts import DocketAlerts, SearchAlerts
-from courtlistener.citation_lookup import CitationLookup
-from courtlistener.client import CourtListener
-from courtlistener.exceptions import CourtListenerAPIError
+from courtlistener.sync_client.client import CourtListener
 
 __all__ = [
-    "CitationLookup",
     "CourtListener",
-    "CourtListenerAPIError",
-    "DocketAlerts",
-    "SearchAlerts",
 ]
