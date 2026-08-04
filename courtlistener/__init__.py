d761776417fe09fa7e1627976dc7119d504e725f
Nathan Dahlberg <nadahlberg@gmail.com>
2026-08-04 15:13:57 -0400

    feat(package): export all client classes to package top

--- courtlistener/__init__.py
+++ courtlistener/__init__.py
@@ -1,7 +1,32 @@
+from courtlistener.async_client.alerts import (
+    AsyncDocketAlerts,
+    AsyncSearchAlerts,
+)
+from courtlistener.async_client.citation_lookup import AsyncCitationLookup
 from courtlistener.async_client.client import AsyncCourtListener
+from courtlistener.async_client.resource import (
+    AsyncResource,
+    AsyncResourceIterator,
+)
+from courtlistener.exceptions import CourtListenerAPIError, InvalidFieldsError
+from courtlistener.sync_client.alerts import DocketAlerts, SearchAlerts
+from courtlistener.sync_client.citation_lookup import CitationLookup
 from courtlistener.sync_client.client import CourtListener
+from courtlistener.sync_client.resource import Resource, ResourceIterator
 
 __all__ = [
+    "AsyncCitationLookup",
     "AsyncCourtListener",
+    "AsyncDocketAlerts",
+    "AsyncResource",
+    "AsyncResourceIterator",
+    "AsyncSearchAlerts",
+    "CitationLookup",
     "CourtListener",
+    "CourtListenerAPIError",
+    "DocketAlerts",
+    "InvalidFieldsError",
+    "Resource",
+    "ResourceIterator",
+    "SearchAlerts",
 ]
